f3206b73ad6ef4164b70690c427e6d51ddf3d19e
arnau-K <deprezsantamaria1@gmail.com>
2025-04-09 09:03:15 +0200

    he tornat a posar l'interrupt però ara les dades es generen al bucle ppal

--- src/main.cpp
+++ src/main.cpp
@@ -23,15 +23,15 @@ volatile bool new_sample_available = false;
 volatile float ecg_sample = 0.0;
 volatile float res_sample = 0.0;
 
-/*Tot això està comentat xq tardava molt fer-ho així i els interrupts petaven l'ESP
+
+//-------------------------- Setup de l'interrupt --------------------------
+
+hw_timer_t* timer;
+
 // Funció per l'interrupt de mostreig de dades ECG
 void IRAM_ATTR onTimer() {
-    // No hauria de fer tot això aquesta funció xq està tardant massa. Al final això igualment desapareixerà
-    ecg_sample = gen.generarSenyalECG();  // Generar mostra (simula mostra arribada de l'ADS)
-    res_sample = gen.generarSenyalRES();  //TODO: Crear aquesta funció a partir del codi den carles
     new_sample_available = true;
-    gen.temps += gen.dt;  // Avança temps simulat
-}*/
+}
 
 // Variables per detectar els pics RR:
 float umbral = 0.6; // Llindar simple (s'hauria de fer automàtic)
@@ -57,55 +57,74 @@ void setup() {
     Serial.begin(BAUD_RATIO);
     delay(1000);
     iniciarBLE(); // Iniciem la comunicació x BLE
+
+    // Config pel timer (d'exemple)
+    timer = timerBegin(0, 80, true);                // 80 prescaler -> 1 tick
+    debugln("timer iniciat");
+    timerAttachInterrupt(timer, &onTimer, true);    // Connectem l'interrupt
+    debugln("interrupt configurat");
+    timerAlarmWrite(timer, 1000000 / gen.fs, true); // Configurar alarma al fs de l'ECG. Va 10 vegades més lent del que hauria xq no peti jeje
+    debugln("alarma OK");
+    timerAlarmEnable(timer);
+    debugln("Timer i interrupt configurat");
 }
 
 void loop() {
-    ecg_sample = gen.generarSenyalECG();  // Generar mostra (simula mostra arribada de l'ADS)
-    res_sample = gen.generarSenyalRES();  //TODO: Crear aquesta funció a partir del codi den carles
 
-    gen.temps += gen.dt;  // Avança temps simulat (crec q això s ha fotut, hauria de fer algo amb millis() ara x simular el delay)
+    if(new_sample_available) {
 
-    debug("dades rebudes");
-    debug(ecg_value);
-    debug(", ");
-    debugln(res_value);
+        noInterrupts(); // Apagar interrupts per copiar dades en memòria compartida
+        float ecg_value = ecg_sample;
+        float res_value = res_sample;
+        new_sample_available = false;
+        interrupts();   // Re-encendre interrupts
 
-    // Buffer cutre per trobar pics de l'ECG
-    anterior = actual;
-    actual = seguent;
-    seguent = ecg_sample;
-
-    if (detectarPicR(actual, anterior, seguent, llindar) == 1) {
-        unsigned long rr = millis() - tempsUltimPic;
-        tempsUltimPic = millis();
-
-        Serial.print("interval RR (ms): ");
-        Serial.println(rr);
-
-        // afegim al vector d'intervals RR
-        afegirRR(&bufferRR, rr);    // Aquesta funció ignora les dades si ja està ple el buffer (TODO: s'hauria de canviar)
-        afegirRR(&bufferTimeRR, tempsUltimPic); // Afegim el temps al seu vector
-    }
-
-    // Interpolem 
-    if(millis() > 120000 && !interpolationDone) { // Comencem a fer interpolacions a partir de dos minuts d'haver pres dades
-        
-        interpolar(&bufferInterRR, &bufferRR, &bufferInterTimeRR, &bufferTimeRR);
-        interpolationDone = true;
+        debug("dades rebudes");
+        debug(ecg_value);
+        debug(", ");
+        debugln(res_value);
     
+        // Buffer cutre per trobar pics de l'ECG
+        anterior = actual;
+        actual = seguent;
+        seguent = ecg_value;
+    
+        if (detectarPicR(actual, anterior, seguent, llindar) == 1) {
+            unsigned long rr = millis() - tempsUltimPic;
+            tempsUltimPic = millis();
+    
+            Serial.print("interval RR (ms): ");
+            Serial.println(rr);
+    
+            // afegim al vector d'intervals RR
+            afegirRR(&bufferRR, rr);    // Aquesta funció ignora les dades si ja està ple el buffer (TODO: s'hauria de canviar)
+            afegirRR(&bufferTimeRR, tempsUltimPic); // Afegim el temps al seu vector
+        }
+    
+        // Interpolem 
+        if(millis() > 120000 && !interpolationDone) { // Comencem a fer interpolacions a partir de dos minuts d'haver pres dades
+            
+            interpolar(&bufferInterRR, &bufferRR, &bufferInterTimeRR, &bufferTimeRR);
+            interpolationDone = true;
+        
+        }
+    
+        // FFT
+        if(interpolationDone){
+    
+            FFTbuffer bufferFFT = crearFFTbuffer(); // Creem un objecte que ens permet gestionar la fft
+            setArrays(bufferInterRR.vec, &bufferFFT); // Introduim les dades de les interpolacions
+            calcularFFT(&bufferFFT);
+            computeStress(&bufferFFT, sns, snp, stress);
+    
+        }
+    
+        // Afegir al paquet BLE i enviar si està ple
+        afegirDadesPaquet(&pBLE_U, ecg_value, res_value);
+
+        // Generem la següent dada
+        ecg_sample = gen.generarSenyalECG();  // Generar mostra (simula mostra arribada de l'ADS)
+        res_sample = gen.generarSenyalRES();  //TODO: Crear aquesta funció a partir del codi den carles
+        gen.temps += gen.dt;  // Avança temps simulat (crec q això s ha fotut, hauria de fer algo amb millis() ara x simular el delay)    
     }
-
-    // FFT
-    if(interpolationDone){
-
-        FFTbuffer bufferFFT = crearFFTbuffer(); // Creem un objecte que ens permet gestionar la fft
-        setArrays(bufferInterRR.vec, &bufferFFT); // Introduim les dades de les interpolacions
-        calcularFFT(&bufferFFT);
-        computeStress(&bufferFFT, sns, snp, stress);
-
-    }
-
-    // Afegir al paquet BLE i enviar si està ple
-    afegirDadesPaquet(&pBLE_U, ecg_sample, res_sample);
-    debugln("afegides al paquet");
 }
